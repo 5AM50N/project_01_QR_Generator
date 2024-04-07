68f92a441affbc33142f9d745150764ccc3de5a0
Aditya <adityakamble.quicklabs@gmail.com>
2024-04-07 12:39:43 +0530

    css not working

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.set("view engine", "ejs");
 app.use(bp.urlencoded({extended: false}));
 app.use(bp.json());
 app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "public"));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/",(req, res) => {
